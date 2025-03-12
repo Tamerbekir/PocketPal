b65e5811ed8cdfe77fa1a722cc55d20e61e82844
Tamerbekir <astoryofame@gmail.com>
2025-03-12 10:54:31 -0400

    Fixed css for Todo list. Readded date

--- client/src/Pages/Todo.tsx
+++ client/src/Pages/Todo.tsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import Form from "react-bootstrap/Form";
 import FloatingLabel from "react-bootstrap/FloatingLabel";
 import InputGroup from "react-bootstrap/InputGroup";
+import DateTimePicker from "react-datetime-picker";
+
 import "../assets/todo.css";
 
 export default function Todo() {
@@ -17,6 +19,7 @@ export default function Todo() {
     notes: "",
   });
 
+  const [date, setDate] = useState(new Date());
   const [submitTodo, setSubmitTodo] = useState<Todo[]>(() => {
     const savedData = localStorage.getItem("submitTodo");
     return savedData ? JSON.parse(savedData) : [];
@@ -71,27 +74,26 @@ export default function Todo() {
             style={{ height: "100px" }}
           />
         </FloatingLabel>
-        {/* <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DatePicker
-            value={todo.date}
-            label="Date"
+        <InputGroup className="mb-3">
+          <Form.Control
+            placeholder="To do"
+            type="date"
             name="date"
+            value={todo.date}
+            onChange={handleTodoChange}
             className="todoDate"
-            onChange={() => handleDateChange}
-            slotProps={{
-              textField: {
-                helperText: "MM/DD/YYYY",
-              },
-            }}
+            aria-describedby="basic-addon1"
           />
-        </LocalizationProvider> */}
-        <button onClick={handleSubmitTodo}>Add</button>
+        </InputGroup>
+        <button className="addBtn" onClick={handleSubmitTodo}>
+          Add
+        </button>
       </div>
       <h4>List</h4>
       <div className="listDiv">
         {submitTodo.map((addedItem, index) => (
           <div key={index}>
-            {/* <p>{addedItem.date}</p> */}
+            <p>{addedItem.date}</p>
             <p>{addedItem.item}</p>
             <p>{addedItem.notes}</p>
             <button
