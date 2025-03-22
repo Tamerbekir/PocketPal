c61cbb90296df9d81a38bb7e28889cc38e59c71a
Tamerbekir <astoryofame@gmail.com>
2025-03-22 19:09:49 -0400

    Added awesomeFont to vite as alias .

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,21 +1,24 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [
     react({
-      jsxRuntime: 'automatic',
+      jsxRuntime: "automatic",
     }),
   ],
+  resolve: {
+    alias: {
+      "@fortawesome/react-fontawesome":
+        "@fortawesome/react-fontawesome/index.es.js",
+    },
+  },
   build: {
-    outDir: '../dist',
+    outDir: "../dist",
     emptyOutDir: true,
   },
   server: {
-    host: '0.0.0.0',
-    port: 3001
-  }
+    host: "0.0.0.0",
+    port: 3001,
+  },
 });
-
-
-
